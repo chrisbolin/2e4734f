beca675ced1f80495b89411b3beb4318193e7147
Chris Bolin <bolin.chris@gmail.com>
2017-08-11 09:45:51 -0600

    simple beesandbombs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, cloneElement, Children } from 'react';
 import { range } from 'lodash';
 
+const { floor, sqrt, pow, cos } = Math;
+
 const prepareChildren = children => Children.toArray(children).filter(el => typeof el !== 'string');
 
 const GridElement = ({index, base, children}) => {
-	const row = Math.floor(index / base);
-	const column = (index % base);
+	const row = (index % base);
+	const column = floor(index / base);
 	const transform = `translate(${row * base},${column * base})`;
 
 	return (
@@ -32,21 +34,46 @@ const Grid = ({ children }) => {
 	);
 };
 
-const Tile = ({ index, x = 0, fill = 'black' }) => (
-	<g>
-		<rect x={x} fill={fill} width={1} height={6} />
-		<rect x={x} fill={fill} width={2} height={4} />
-		<rect x={x} fill={fill} width={4} height={1} />
-	</g>
-);
+const Tile = ({ frame, index, row, column }) => {
+	const duration = 50;
+	const distance = sqrt( pow(row-4.5, 2) + pow(column-4.5, 2) );
+	let progress = 0;
+
+	const adjustedFrame = (15 * distance + 1.5 * frame) % 200;
+	if (adjustedFrame > 100) progress = (adjustedFrame - 100)/duration;
+	if (adjustedFrame > (100 + duration)) progress = 1;
+
+	const rotate = 270 * progress;
+	return (
+		<g transform={`rotate(${rotate} 6 6)`}>
+			<rect x={2} y={5.5} width={8} height={1} fill="grey" />
+			<rect x={5.5} y={2} width={1} height={8} fill="grey" />
+			<rect x={5.5} y={5.5} width={1} height={1} fill="white" />
+		</g>
+	);
+};
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			frame: 0,
+		};
+	}
+	componentDidMount() {
+		this.tick();
+	}
+	tick = () => {
+		this.setState({
+			frame: this.state.frame + 1
+		});
+		window.requestAnimationFrame(this.tick);
+	}
   render() {
     return (
       <div>
 				<Grid>
-					<Tile />
-					<Tile x={5} fill="grey" />
+					<Tile frame={this.state.frame}/>
 				</Grid>
       </div>
     );
