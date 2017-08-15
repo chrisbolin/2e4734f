ea3d92cf620459a0626e33effe487b1e18c2ba35
Chris Bolin <bolin.chris@gmail.com>
2017-08-14 18:19:56 -0600

    split tyler lib apart

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import React, { Component, cloneElement, Children } from 'react';
-import { range } from 'lodash';
+import React, { Component } from 'react';
+import { Grid } from './tyler';
 
-const { floor, sqrt, pow, cos, PI } = Math;
-
-const prepareChildren = children => Children.toArray(children).filter(el => typeof el !== 'string');
-
-const GridElement = ({index, base, children}) => {
-	const row = (index % base);
-	const column = floor(index / base);
-	const transform = `translate(${row * base + base/2},${column * base + base/2})`;
-
-	return (
-		<g transform={transform}>
-			{children.map(child => cloneElement(child, { index, row, column }))}
-		</g>
-	);
-};
-
-const Grid = ({ children }) => {
-	const viewBox = `0 0 100 100`;
-	const base = 10;
-	const childrenElements = prepareChildren(children);
-	return (
-		<svg viewBox={viewBox}>
-			{
-				range(100).map(index =>
-					<GridElement index={index} base={base} key={index}>
-						{childrenElements}
-					</GridElement>
-				)
-			}
-		</svg>
-	);
-};
+const { sqrt, pow, cos, PI } = Math;
 
 const Tile = ({ frame, index, row, column }) => {
 	const time = {
---
+++ src/tyler.js
@@ -0,0 +1,35 @@
+import React, { cloneElement, Children } from 'react';
+import { range } from 'lodash';
+
+const { floor } = Math;
+
+const prepareChildren = children => Children.toArray(children).filter(el => typeof el !== 'string');
+
+const GridElement = ({index, base, children}) => {
+	const row = (index % base);
+	const column = floor(index / base);
+	const transform = `translate(${row * base + base/2},${column * base + base/2})`;
+
+	return (
+		<g transform={transform}>
+			{children.map(child => cloneElement(child, { index, row, column }))}
+		</g>
+	);
+};
+
+export const Grid = ({ children }) => {
+	const viewBox = `0 0 100 100`;
+	const base = 10;
+	const childrenElements = prepareChildren(children);
+	return (
+		<svg viewBox={viewBox}>
+			{
+				range(100).map(index =>
+					<GridElement index={index} base={base} key={index}>
+						{childrenElements}
+					</GridElement>
+				)
+			}
+		</svg>
+	);
+};
